f2f9ac09b99f42c499a6527ad994226ebf4bfbe2
Mac May <tmcpbmay@gmail.com>
2020-12-04 16:06:01 -0500

    shay fixes heroku

--- db/migrate/20201204011241_update_notes.rb
+++ db/migrate/20201204011241_update_notes.rb
@@ -1,4 +1,8 @@
 class UpdateNotes < ActiveRecord::Migration[6.0]
   def change
+    add_reference :weapons, :suspect, index:true
+    add_reference :alibis, :suspect, index:true
+    add_foreign_key :weapons, :suspects
+    add_foreign_key :alibis, :suspects
   end
 end
