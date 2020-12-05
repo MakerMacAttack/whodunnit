a7048cafcdff77737e6e034131837bc22abd5046
Mac May <tmcpbmay@gmail.com>
2020-12-05 09:40:14 -0500

    I believe I meet MVP now

--- client/src/screens/Notebook/Notebook.jsx
+++ client/src/screens/Notebook/Notebook.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Note from "../../components/Note/Note";
 import NoteCreate from "../../components/NoteCreate/NoteCreate";
 import "./Notebook.css";
 
 export default function Notebook({ notes, trigger, currentUser }) {
-  return (
+  useEffect(() => {
+    trigger((prevState) => !prevState);
+    // eslint-disable-next-line
+  }, []);
+
+  return notes.length === 0 ? (
+    <h1>Notebook Loading...</h1>
+  ) : (
     <div>
       {notes.map((note, idx) => (
         <Note key={idx} note={note} />
