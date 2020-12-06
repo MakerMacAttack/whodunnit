ce67cbf1df1192a384d39fb0bc5bb3348520a468
Mac May <tmcpbmay@gmail.com>
2020-12-06 16:30:20 -0500

    header and footer of first breakpoint

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -4,10 +4,12 @@ import "./Footer.css";
 export default function Footer(props) {
   return (
     <div id="footer">
-      <p>Tech Media Management &trade;</p>
-      <div className="foot-button">
-        <button>GitHub</button>
-        <button>LinkedIn</button>
+      <div id="foot-content">
+        <p>Tech Media Management &trade;</p>
+        <div className="foot-button">
+          <button>GitHub</button>
+          <button>LinkedIn</button>
+        </div>
       </div>
     </div>
   );
